a097141d6abd3fb81b84a572361d47841ad3c3b9
DayOS <43686351+Day-OS@users.noreply.github.com>
2021-12-23 14:15:47 -0300

    Initial Code commit

---
+++ src/lib.cpp
@@ -0,0 +1,72 @@
+#include "lib.h"
+#include "iostream"
+#include "vector"
+#include "string.h"
+
+
+
+VTNCRW::VTNCRW(/* args */) 
+{
+    this->a = 's';
+    std::cout << "I'M LOADED!" << std::endl;
+}
+
+ void readChunk(std::vector<unsigned char> file, int startingindex, int bytesize, unsigned char *output, int &lastaddress, bool isString = false){
+     
+    for (size_t i = startingindex; i < startingindex + bytesize; i++)
+    { 
+        std::cout << std::hex << i  << " (" << int(*(output + i)) << ") |";
+        memset(output + i, file[i], 1);
+        std::cout << std::hex << i  << " (" << int(*(output + i)) << ") |" << std::endl;
+    }
+    if (isString)
+    {
+        lastaddress -= 1;
+        memset(output + bytesize-1, 0, 1);
+    }
+    
+    lastaddress += bytesize;
+    return;
+}
+
+VTNCFile VTNCRW::read(std::vector<unsigned char> file)
+{
+    VTNCFile output;
+    u8c _TAGNeeded [5] = "VTNC";
+    u8c TAG [5] = {0};
+    output.layersQuantity;
+    output.layerKeys[U8Max];
+    output.layersResolution[U8Max];
+    output.colorsQuantity;
+    output.Colors [U8Max];
+    output.framesQuantity;
+    output.framesArray [U8Max];
+
+    int size = 4;
+    int blockOffset = 0;
+
+    
+    readChunk(file, 0, 5, TAG, blockOffset, true);
+    readChunk(file, blockOffset, 1, &output.layersQuantity, blockOffset);
+
+    std::cout << std::endl << int(output.layersQuantity) << std::endl ;
+    //readChunk(file, blockOffset, 1, &output.layersQuantity, blockOffset);
+    /*
+    for (size_t i = 0; i < 4; i++)
+    { 
+        TAG[i] = file[i];
+        //std::cout << std::hex << int(file[i]) << "|";
+    }
+    */
+    if (memcmp(TAG, _TAGNeeded, 5) == 0)
+    {
+        output.isFile = true;
+    }
+    
+   
+    /*
+    
+    */
+    
+    return output;
+}
---
+++ src/lib.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "vector"
+
+
+typedef unsigned char u8c;
+typedef unsigned short int u16c;
+#define U8Max 0xFF
+#define U16Max 0xFFFF
+#define U24Max 0xFFFFFF
+
+
+class Resolution{
+    u16c x;
+    u16c y;
+};
+class RGB{
+    u8c R;
+    u8c G;
+    u8c B;
+};
+class Frame {
+    u8c Pixels [U8Max];
+    u16c msDuration;
+};
+
+class VTNCFile
+{
+public:
+    bool isFile = false;
+    u8c layersQuantity;
+    u8c layerKeys[U8Max];
+    Resolution layersResolution[U8Max];
+    u8c colorsQuantity;
+    RGB Colors [U8Max];
+    u8c framesQuantity;
+    Frame framesArray [U8Max];
+};
+
+class VTNCRW
+{
+private:
+    /* data */
+public:
+    VTNCRW(/* args */);
+    VTNCFile read(std::vector<unsigned char> file);
+    char a;
+};
